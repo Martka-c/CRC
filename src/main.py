1cccfac8274f44ed8ebd45196260830b4e31b48d
Marta <marta.stiebler@ing.com>
2026-04-19 21:21:02 +0200

    firestore

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from google.cloud import firestore
 
 app = FastAPI()
 
@@ -6,3 +7,33 @@ app = FastAPI()
 @app.get("/")
 def hello_world():
     return {"message": "OK"}
+
+@app.post("/nowy_dokument")
+def create_document():
+    # Application Default credentials are automatically created.
+    db = firestore.Client()
+    cities = db.collection("cities")
+
+    sf_landmarks = cities.document("SF").collection("landmarks")
+    sf_landmarks.document().set({"name": "Golden Gate Bridge", "type": "bridge"})
+    sf_landmarks.document().set({"name": "Legion of Honor", "type": "museum"})
+    la_landmarks = cities.document("LA").collection("landmarks")
+    la_landmarks.document().set({"name": "Griffith Park", "type": "park"})
+    la_landmarks.document().set({"name": "The Getty", "type": "museum"})
+    dc_landmarks = cities.document("DC").collection("landmarks")
+    dc_landmarks.document().set({"name": "Lincoln Memorial", "type": "memorial"})
+    dc_landmarks.document().set(
+        {"name": "National Air and Space Museum", "type": "museum"}
+    )
+    tok_landmarks = cities.document("TOK").collection("landmarks")
+    tok_landmarks.document().set({"name": "Ueno Park", "type": "park"})
+    tok_landmarks.document().set(
+        {"name": "National Museum of Nature and Science", "type": "museum"}
+    )
+    bj_landmarks = cities.document("BJ").collection("landmarks")
+    bj_landmarks.document().set({"name": "Jingshan Park", "type": "park"})
+    bj_landmarks.document().set(
+        {"name": "Beijing Ancient Observatory", "type": "museum"}
+    )
+
+    return {"message": "Document created"}
